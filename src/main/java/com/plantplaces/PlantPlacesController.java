5d4e56922f373163456097f6a3b54f144cdc7487
Ruairi Blake <Rblake0601@gmail.com>
2021-09-23 14:09:16 -0400

    Service stub, DTO and controller

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,14 +1,53 @@
 package com.plantplaces;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.plantplaces.service.SpecimenServiceStub;
+/**
+ * Handle the /start endpoint
+ *
+ */
 @Controller
 public class PlantPlacesController {
+	
+	@Autowired
+	private SpecimenServiceStub specimenServiceStub;
 
-	@RequestMapping("/start")
-	public String start() {
-		
+	@RequestMapping(value = "/start", method = RequestMethod.GET)
+	public String read() {
+		specimenServiceStub.fetchBy(43);
+		return "start";
+	}
+
+	@RequestMapping(value = "/start", method = RequestMethod.GET, headers = {"content-type=text/json"})
+	public String readJson() {
+		return "start";
+	}
+
+	@RequestMapping(value = "/start", method = RequestMethod.GET, params = {"loyalty=blue"})
+	public String readBlue() {
+		return "start";
+	}
+
+	@RequestMapping(value = "/start", method = RequestMethod.GET, params = {"loyalty=silver"})
+	public String readSilver() {
+		return "start";
+	}
+
+	@PostMapping("/start")
+	public String create() {
+		return "start";
+	}
+
+	/**
+	 * Handle the / endpoint
+	 */
+	@RequestMapping("/")
+	public String index() {
 		return "start";
 	}
 }
